aaeffb69a715cd6a923fb296f5450939536b5e20
Nedu Robert <nedumrobert@gmail.com>
2018-05-16 23:26:00 +0100

    added model for user schema

---
+++ server/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true, 
+        unique: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 12
+    }
+});
+
+userSchema.pre('save', function(next) {
+    bcrypt.hash(this.password, 12, (err, hash) => {
+        if(err) {
+            return next(err);
+        }
+        this.password = hash;
+        return next();
+    });
+});
+
+userSchema.methods.validatePassword = function(password) {
+    return bcrypt.compare(password, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
