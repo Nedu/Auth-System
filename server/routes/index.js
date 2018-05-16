96a14b078bfa2647262069a9d1c2b6f0eb605646
Nedu Robert <nedumrobert@gmail.com>
2018-05-17 00:03:33 +0100

    added endpoint for logging in users using JWT

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,11 +2,40 @@ const routes = require('express').Router();
 const jwt = require('jsonwebtoken');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
-const JwtStrategy = require('passport-jwt').Strategy;
-const { ExtractJwt } = require('passport-jwt');
 
 const User = require('../models/User');
 const secret = 'hdhscuehvuchfudvh';
+
+const localStrategy = new LocalStrategy(function(username, password, done) {
+  User.findOne({ username })
+    .then(user => {
+      if (!user) {
+        done(null, false);
+      } else {
+        user
+          .validatePassword(password)
+          .then(isValid => {
+            if (isValid) {
+              const { _id, username } = user;
+              return done(null, { _id, username }); 
+            } else {
+              return done(null, false);
+            }
+          })
+          .catch(err => {
+            return done(err);
+          });
+      }
+    })
+    .catch(err => done(err));
+});
+
+// passport global middleware
+passport.use(localStrategy);
+
+// passport local middleware
+const passportOptions = { session: false };
+const authenticate = passport.authenticate('local', passportOptions);
 
 function makeToken(user) {
     const timestamp = new Date().getTime();
@@ -33,6 +62,10 @@ routes.post('/register', (req, res) => {
     .catch(err => {
         res.status(500).json(err);
     })
+})
+
+routes.post('/login', authenticate, (req, res) => {
+    res.status(200).json({ token: makeToken(req.user), user: req.user})
 })
 
 module.exports = routes;
