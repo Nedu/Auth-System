48f6590b5ba19a0257c4750a5f0aa4bf4b4f887c
Nedu Robert <nedumrobert@gmail.com>
2018-05-16 22:33:02 +0100

    refactored out the middlewares into their own file

--- server/server.js
+++ server/server.js
@@ -1,21 +1,14 @@
 const express = require('express');
-const morgan = require('morgan');
-const helmet = require('helmet');
 const mongoose = require('mongoose');
-const cors = require('cors');
-
 const server = express();
+
+const setupMiddleware = require('./config/middleware')(server);
 
 mongoose.connect(`mongodb://localhost/auth`).then(conn => {
     console.log(`\n=== Successfully connected to database ==\n`);
 }).catch(err => {
     console.log(`\n=== Error connecting to database: ${err} ===\n`)
 });
-
-server.use(helmet());
-server.use(morgan('dev'));
-server.use(express.json());
-server.use(cors());
 
 server.get('/', (req, res) => {
     res.json({ api: 'up and running!' });
